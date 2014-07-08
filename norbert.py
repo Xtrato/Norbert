5048d050b5c7248fc5e6f0f690c9a013de3684e6
Ryanb4084 <ryanb4084@gmail.com>
2014-07-08 13:29:56 +0100

    Added movement functions

--- norbert.py
+++ norbert.py
@@ -1,17 +1,54 @@
 import Image
+import RPi.GPIO as gpio
+import time
 
-#Opens the camera image. Compresses it to only include 20 colours.
-cameraImage = Image.open('boxwithball.jpg').convert('P', palette=Image.ADAPTIVE, colors=20).convert('RGB')
-#Saves the image (only needed for testing)
-cameraImage.save('test.png')
-#Saves the RGB values of the colours in the image to a list
-imageColours = cameraImage.getcolors(256)
+def takePicture():
+    cameraImage = Image.open('boxwithball.jpg').convert('P', palette=Image.ADAPTIVE, colors=20).convert('RGB')
+    cameraImage.save('test.png')
+    imageColours = cameraImage.getcolors(256)
+    for colour in imageColours:
+        if colour[1][0] > 140 and colour[1][1] < 120 and colour[1][2] < 120:
+            print 'Ball in picture'
 
-#Iterates through the list of colours in the image
-for colour in imageColours:
-    #If the colour is red (The call is in the image)
-    if colour[1][0] > 140 and colour[1][1] < 120 and colour[1][2] < 120:
-        #Prints a message
-        print 'Ball in picture'
+def forward(seconds):
+    startTime = time.time()
+    while time.time() < startTime + seconds:
+        gpio.output(16, False)
+        gpio.output(18, True)
+        gpio.output(24, True)
+        gpio.output(22, False)
+        time.sleep(0.01)
 
-print 'End Line Geoffrey Robot'
+def backwards(seconds):
+    gpio.output(16, True)
+    gpio.output(18, False)
+    gpio.output(24, False)
+    gpio.output(22, True)
+    time.sleep(seconds)
+
+def stop():
+    gpio.output(16, False)
+    gpio.output(18, False)
+    gpio.output(24, False)
+    gpio.output(22, False)
+    time.sleep(seconds)
+
+gpio.setmode(gpio.BOARD)
+gpio.setup(7, gpio.OUT)
+gpio.setup(11, gpio.OUT)
+gpio.setup(16, gpio.OUT)
+gpio.setup(18, gpio.OUT)
+gpio.setup(13, gpio.OUT)
+gpio.setup(15, gpio.OUT)
+gpio.setup(22, gpio.OUT)
+gpio.setup(24, gpio.OUT)
+
+gpio.output(7, True)
+gpio.output(11, True)
+gpio.output(13, True)
+gpio.output(15, True)
+
+forward(7)
+stop()
+
+print 'End Line Norbert Robot'
